fb455a0926f9f60c4efe2db716386f83928ca129
khang <mkhangle20@gmail.com>
2023-09-13 18:19:45 -0400

    Update main example for most basic usage

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,13 @@ extern crate hldemo;
 use std::fs::File;
 use std::io::Read;
 
-use demo_doer::{example, netmsg_rewrite_test, superimpose};
+use demo_doer::{
+    add_debug, example, netmsg_rewrite_test,
+    remove_te_textmessage::remove_te_textmesasge,
+    superimpose::{self, superimpose},
+};
 
-use crate::demo_doer::{add_speedometer, remove_entities};
+use crate::demo_doer::{add_speedometer, remove_entities, superimpose::superimpose_folder};
 
 mod demo_doer;
 mod types;
@@ -13,45 +17,7 @@ mod utils;
 mod writer;
 
 fn main() {
-    // let mut bytes = Vec::new();
-    // let mut f = File::open("./example/hldm.dem").unwrap();
-    // // let mut f = File::open("./test.dem").unwrap();
-
-    // f.read_to_end(&mut bytes).unwrap();
-
-    // let mut demo = hldemo::Demo::parse(&bytes).unwrap();
-    // example::example(&mut demo);
-    // // example::netmsg_parse_write_parse_extra(&mut demo);
-    // // netmsg_rewrite_test::netmsg_rewrite_test(&mut demo);
-    // // example::netmsg_parse(&mut demo);
-    // // example::netmsg_parse_write_parse(&mut demo);
-    // example::netmsg_parse_write(&mut demo);
-    // // add_speedometer::add_speed_o_meter(&mut demo);
-    // write_demo!("test.dem", demo);
-
-    superimpose::superimpose(
-        "./example/gold.dem".to_string(),
-        vec![
-            ("./example/gold2.dem".to_string(), 0.),
-            // ("./example/gold3.dem".to_string(), 0.),
-            // ("./example/gold4.dem".to_string(), 0.),
-            // ("./example/gold5.dem".to_string(), 0.),
-        ],
-    );
-
-    // netmsg_rewrite_test::netmsg_rewrite_test("./example/gold.dem");
-
-    // remove_entities::remove_entities(&mut demo, vec![68u16]);
-    // write_demo!("test.dem", demo);
-
-    // let mut bytes = Vec::new();
-    // let mut f = File::open("./test.dem").unwrap();
-    // f.read_to_end(&mut bytes);
-
-    // let mut demo = hldemo::Demo::parse(&bytes).unwrap();
-    // // example::example(&mut demo);
-    // // netmsg_rewrite_test::netmsg_rewrite_test(&mut demo);
-    // example::print_netmsg(&mut demo);
-
-    // // write_demo!("test.dem", demo);
+    let mut demo = open_demo!("whatdemo.dem");
+    add_speedometer::add_speed_o_meter(&mut demo);
+    write_demo!("out.dem", demo)
 }
