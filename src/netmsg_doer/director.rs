559afaa9aa8504c7117134803ea7c6b086cb7a0a
khang <mkhangle20@gmail.com>
2023-09-25 22:26:57 -0400

    parser: fix SvcDirector wrong message length
    It appears before but I thought it is demo's fault.

--- src/netmsg_doer/director.rs
+++ src/netmsg_doer/director.rs
@@ -3,14 +3,17 @@ use super::*;
 pub struct Director {}
 impl<'a> NetMsgDoer<'a, SvcDirector<'a>> for Director {
     fn parse(i: &'a [u8]) -> IResult<&'a [u8], SvcDirector<'a>> {
-        map(
-            tuple((le_u8, le_u8, null_string)),
-            |(length, flag, message)| SvcDirector {
+        let (i, (length, flag)) = tuple((le_u8, le_u8))(i)?;
+        let (i, message) = take(length - 1)(i)?;
+
+        Ok((
+            i,
+            SvcDirector {
                 length,
                 flag,
                 message,
             },
-        )(i)
+        ))
     }
 
     fn write(i: SvcDirector) -> Vec<u8> {
