af6d2d164d3cfb0414e002663effcd9c0f3aace6
khang <mkhangle20@gmail.com>
2024-01-06 16:57:27 -0500

    refactor get_ghost to singular

--- src/demo_doer/get_ghost/mod.rs
+++ src/demo_doer/get_ghost/mod.rs
@@ -20,13 +20,36 @@ mod surf_gateway;
 
 mod types;
 
-pub fn get_ghost(others: &Vec<(String, f32)>) -> Vec<GhostInfo> {
+pub fn get_ghost(filename: &str, offset: &f32) -> GhostInfo {
+    let pathbuf = PathBuf::from(filename);
+
+    std::io::stdout().flush().unwrap();
+
+    let ghost = if pathbuf.to_str().unwrap().ends_with(".dem") {
+        let demo = open_demo!(filename);
+        demo_ghost_parse(filename, demo, *offset)
+    } else if pathbuf.to_str().unwrap().ends_with(".simen.txt") {
+        // Either this, or use enum in main file.
+        simen_ghost_parse(filename.to_owned(), *offset)
+    } else if pathbuf.to_str().unwrap().ends_with(".sg.json") {
+        // Surf Gateway
+        surf_gateway_ghost_parse(filename.to_owned(), *offset)
+    } else if pathbuf.to_str().unwrap().ends_with(".rj.json") {
+        // Romanian-Jumprs
+        romanian_jumpers_ghost_parse(filename.to_owned(), *offset)
+    } else {
+        println!("");
+        panic!("File \"{}\" does not use supported extension.", filename);
+    };
+
+    ghost
+}
+
+pub fn get_ghosts(others: &Vec<(String, f32)>) -> Vec<GhostInfo> {
     others
         .iter()
         .enumerate()
         .map(|(index, (filename, offset))| {
-            let pathbuf = PathBuf::from(filename);
-
             print!(
                 "\rParsing {} ({}/{})    ",
                 filename,
@@ -35,24 +58,7 @@ pub fn get_ghost(others: &Vec<(String, f32)>) -> Vec<GhostInfo> {
             );
             std::io::stdout().flush().unwrap();
 
-            let ghost = if pathbuf.to_str().unwrap().ends_with(".dem") {
-                let demo = open_demo!(filename);
-                demo_ghost_parse(filename, demo, *offset)
-            } else if pathbuf.to_str().unwrap().ends_with(".simen.txt") {
-                // Either this, or use enum in main file.
-                simen_ghost_parse(filename.to_owned(), *offset)
-            } else if pathbuf.to_str().unwrap().ends_with(".sg.json") {
-                // Surf Gateway
-                surf_gateway_ghost_parse(filename.to_owned(), *offset)
-            } else if pathbuf.to_str().unwrap().ends_with(".rj.json") {
-                // Romanian-Jumprs
-                romanian_jumpers_ghost_parse(filename.to_owned(), *offset)
-            } else {
-                println!("");
-                panic!("File \"{}\" does not use supported extension.", filename);
-            };
-
-            ghost
+            get_ghost(filename, offset)
         })
         .collect()
 }
